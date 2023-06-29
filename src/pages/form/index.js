e57ffd293050a2a05fdc207542cc20dd1e89c42d
logan-milkv <logan@milkv.io>
2023-06-29 16:25:48 +0800

    Add donation form in Chinese

--- src/pages/form/index.js
+++ src/pages/form/index.js
@@ -3,7 +3,12 @@ import Layout from '@theme/Layout';
 import axios from 'axios';
 import styles from "./index.module.css"
 
+import Translate from '@docusaurus/Translate';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+
 export default () => {
+    const currentLanguage = useBaseUrl('/');
+
     const [userName, setUserName] = useState('')
     const [userEmail, setUserEmail] = useState('')
     const [county, setCounty] = useState('')
@@ -48,30 +53,30 @@ export default () => {
     return (
         <Layout>
             <div className={styles.box}>
-                <h1>Milk-V Donation Application Form</h1>
+                <h1>Milk-V <Translate id='Donation.title' /></h1>
                 <form className={styles.formBox} onSubmit={handleSubmit}>
                     <div className={styles.page}>
                         <div className={styles.title}>
                             <img src='/form/icons-01.png' />
-                            <h1>Personal Information</h1>
+                            <h1><Translate id='Donation.title.Information' /></h1>
                         </div>
                         <div className={styles.inpBox}>
                             <label>
-                                <div><span className={styles.red}>*</span> Your Name :</div>
-                                <input type="text" value={userName} onChange={(e) => setUserName(e.target.value)} required placeholder='Your Name' />
+                                <div><span className={styles.red}>*</span><Translate id='contact.title.name' /> :</div>
+                                <input type="text" value={userName} onChange={(e) => setUserName(e.target.value)} required placeholder={currentLanguage === '/' ? 'Your Name' : '你的名字'} />
                             </label>
                             <label>
-                                <div> <span className={styles.red}>*</span> Email Address :</div>
-                                <input type="email" value={userEmail} onChange={(e) => setUserEmail(e.target.value)} required placeholder='Your Email Address' />
+                                <div> <span className={styles.red}>*</span><Translate id='contact.title.email' /> :</div>
+                                <input type="email" value={userEmail} onChange={(e) => setUserEmail(e.target.value)} required placeholder={currentLanguage === '/' ? 'Enter your email' : '请输入你的电子邮箱'} />
                             </label>
                         </div>
                         <div className={styles.inpBox2}>
                             <label>
-                                <div>Organization/Affiliation (if applicable) :</div>
+                                <div><Translate id='Donation.title.Information.Organization' /> :</div>
                                 <input type="text" value={organization} onChange={(e) => setOrganization(e.target.value)} />
                             </label>
                             <label>
-                                <div>Website (if applicable) :</div>
+                                <div><Translate id='Donation.title.Information.web' /> :</div>
                                 <input type="text" value={web} onChange={(e) => setWeb(e.target.value)} />
                             </label>
                         </div>
@@ -79,14 +84,14 @@ export default () => {
                     <div className={styles.page}>
                         <div className={styles.title}>
                             <img src='/form/icons-03.png' />
-                            <h1>Project Information</h1>
+                            <h1><Translate id='Donation.title.Project' /></h1>
                         </div>
                         <label>
-                            <div><span className={styles.red}>*</span> Project Name :</div>
+                            <div><span className={styles.red}>*</span><Translate id='Donation.title.Project.name' /> :</div>
                             <input type="text" value={project} required onChange={(e) => setProject(e.target.value)} />
                         </label>
                         <label>
-                            <div><span className={styles.red}>*</span>Hardware :</div>
+                            <div><span className={styles.red}>*</span><Translate id='duo.info.text.Hardware' /> :</div>
                             <select id="contact-method" name="contact-method" value={description2} onChange={contactChange} >
                                 <option value="DUO">Duo</option>
                                 <option value="Pioneer">Pioneer</option>
@@ -94,45 +99,45 @@ export default () => {
                             </select>
                         </label>
                         <label>
-                            <div><span className={styles.red}>*</span>Project Description :</div>
+                            <div><span className={styles.red}>*</span><Translate id='Donation.title.Project.Description' /> :</div>
                             <textarea value={description} required onChange={(e) => setDescription(e.target.value)} />
                         </label>
                     </div>
                     <div className={styles.page}>
                         <div className={styles.title}>
                             <img src='/form/icons-02.png' />
-                            <h1>Shipping Information</h1>
+                            <h1><Translate id='Donation.title.Shipping' /></h1>
                         </div>
                         <label>
-                            <div>Country of Residence:</div>
+                            <div><Translate id='Donation.title.Shipping.Country' /> :</div>
                             <input type="text" value={county} onChange={(e) => setCounty(e.target.value)} />
                         </label>
                         <label>
-                            <div>Phone Number:</div>
+                            <div><Translate id='Donation.title.Shipping.Phone' /> :</div>
                             <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} />
                         </label>
-                        <p>Your phone number will be used solely for communication related to the donation process. We respect your privacy and will ensure the confidentiality of your contact information.</p>
+                        <p><Translate id='Donation.title.Shipping.Phone.process' /></p>
                         <label>
-                            <div>Shipping Address:</div>
+                            <div><Translate id='Donation.title.Shipping.addess' /> :</div>
                             <textarea value={address} onChange={(e) => setAddress(e.target.value)} />
                         </label>
-                        <p>Please note that your shipping address will be used solely for the purpose of delivering the donated hardware. We value your privacy and will handle your information with utmost care.</p>
+                        <p><Translate id='Donation.title.Shipping.addess.process' /></p>
                     </div>
-                    <button type="submit" className={styles.formBtn} disabled={formBtn} >Send</button>
+                    <button type="submit" className={styles.formBtn} disabled={formBtn} ><Translate id='contact.title.send' /></button>
                 </form>
             </div>
             <div className={styles.pop} style={{ display: `${flag ? "flex" : 'none'}` }}>
                 <div className={styles.popBox}>
-                    <h1>😄Send successfully</h1>
-                    <p>Please do not send duplicate</p>
-                    <div className={styles.ok} onClick={() => { setFlag(false) }}>Yes</div>
+                    <h1>😄<Translate id='contact.send.res' /></h1>
+                    <p><Translate id='contact.send.res.info' /></p>
+                    <div className={styles.ok} onClick={() => { setFlag(false) }}><Translate id='contact.send.close' /></div>
                 </div>
             </div>
             <div className={styles.pop} style={{ display: `${flag2 ? "flex" : 'none'}` }}>
                 <div className={styles.popBox}>
-                    <h1>😭Sending failure</h1>
-                    <p>There is a problem with the server, it is being fixed, please do not send repeatedly</p>
-                    <div className={styles.ok} onClick={() => { setFlag2(false) }}>Yes</div>
+                    <h1>😭<Translate id='contact.send.err' /></h1>
+                    <p style={{ color: 'red' }}><Translate id='contact.send.err.info' /></p>
+                    <div className={styles.ok} onClick={() => { setFlag2(false) }}><Translate id='contact.send.close' /></div>
                 </div>
             </div>
         </Layout>
