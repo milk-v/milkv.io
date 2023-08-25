0b6e5468d3f22c0b9d22ed7aad3a3ed6bcb06cdb
logan-milkv <logan@milkv.io>
2023-08-25 09:55:05 +0800

    Start of new homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,25 @@ import ContactBar from "../components/ContactBar"
 
 
 import Translate from '@docusaurus/Translate';
+const Duo_list = () => {
+  return (
+    <>
 
-function HomepageHeader() {
+
+    </>
+  )
+}
+
+
+
+const Home_web = () => {
   const [duo, setDuo] = useState(false)
   const [pioneer, setPioneer] = useState(false)
   const [mars, setMars] = useState(false)
   const [buy, setBuy] = useState(false)
+
+
+  const [index, setIndex] = useState(1)
 
   const buyStart = () => {
     setBuy(false)
@@ -34,8 +47,25 @@ function HomepageHeader() {
       <BuyPop flag={pioneer} module={pioneerStart} type='pioneer' />
       <BuyPop flag={mars} module={marsStart} type='mars' />
       <BuyPop flag={buy} module={buyStart} type='home' />
+      <div className={styles.home_header}>
+        <div className={styles.header_title}>
+          <h1>"As many RISC-V as the <br></br> stars in the Milky Way"</h1>
+          <div className={styles.button_use}>
+            <Link href=''>Comunity</Link>
+            <Link href=''>Learn More</Link>
+          </div>
+        </div>
+      </div>
+      <div className={styles.black_shore}>
+        <ul className={styles.tab}>
+          <li styles><p>Duo</p></li>
+          <li><p>Pioneer</p></li>
+          <li><p>Mars</p></li>
+          <li><p>Meles</p></li>
+          <li><p>Vega</p></li>
+        </ul>
+      </div>
     </>
-
   )
 }
 
@@ -43,7 +73,7 @@ export default function Home() {
   return (
     <>
       <Layout>
-        <HomepageHeader />
+        <Home_web />
         <Footer></Footer>
       </Layout>
     </>
