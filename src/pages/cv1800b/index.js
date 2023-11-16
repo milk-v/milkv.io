4a209bc0b09d39dae9e19aa69ff166542a01040e
logan-milkv <logan@milkv.io>
2023-11-16 18:33:36 +0800

    cv1800b Jump

---
+++ src/pages/cv1800b/index.js
@@ -0,0 +1,9 @@
+import React, { useEffect } from "react";
+
+export default () => {
+    useEffect(() => {
+        window.location.href = 'https://github.com/milkv-duo/duo-hardware/tree/main/CV1800B'
+    }, [])
+
+    return <></>
+}
