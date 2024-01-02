fad6538f8b986873c569a4b0d964232e218328e9
logan-milkv <logan@milkv.io>
2024-01-02 18:49:46 +0800

    Add Image Component

---
+++ src/theme/Image/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './index.module.css'
+import clsx from 'clsx';
+
+
+const Image = (props) => {
+    const { src, minWidth, maxWidth, align } = props
+    let alignment
+
+    switch (align) {
+        case 'center':
+            alignment = styles.doc_img_center
+            break;
+        case 'right':
+            alignment = styles.doc_img_right
+            break;
+        default:
+            break;
+    }
+
+    return <div className={clsx(alignment, styles.docs_img_d)}>
+        <img src={src} style={{ minWidth: minWidth, maxWidth: maxWidth, }} />
+    </div>
+}
+
+Image.prototype = {
+    src: PropTypes.string.isRequired,
+    minWidth: PropTypes.string,
+    maxWidth: PropTypes.string,
+    align: PropTypes.oneOf(['center', 'left', 'right']),
+}
+Image.defaultProps = {
+    maxWidth: '100%',
+    minWidth: '20%',
+    align: 'left',
+}
+
+
+export default Image
---
+++ src/theme/MDXComponents.js
@@ -0,0 +1,9 @@
+
+import MDXComponents from '@theme-original/MDXComponents';
+import Image from './Image';
+
+
+export default {
+    ...MDXComponents,
+    Image
+};
